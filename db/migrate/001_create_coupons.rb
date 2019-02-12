d5d0a9bfb095cc53738e1b20139b0f090ae0ab34
Joey Cho <cjy12182000@naver.com>
2019-02-12 05:32:43 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_coupons.rb
+++ db/migrate/001_create_coupons.rb
@@ -3,7 +3,6 @@ class CreateCoupons < ActiveRecord::Migration
      create_table :coupons do |t|
       t.string :coupon_code
       t.string :store
-      t.string :content
 
       t.timestamps null: false
     end
